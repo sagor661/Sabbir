f6947981b8b79fb5b1b11388591709ccf95e6396
shaahl <hgan60766@gmail.com>
2025-06-23 12:24:33 +0600

    Update ilove.js

--- scripts/commands/ilove.js
+++ scripts/commands/ilove.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("I love you")==0 || (event.body.indexOf("i love you")==0 || (event.body.indexOf("I love u")==0 || (event.body.indexOf("I love you")==0)))) {
 		var msg = {
-				body: "Hmm... বস নয়নও তোমাকে ভালোবাসে😇😻 :))"
+				body: "Hmm... বস ⃝—͟͟͞͞ 𝐂.𝐄.𝐎⸙𝐒𝐇ꫝꫝ𝐇𝐋𝐎𝐌𓆪___//😈🙂🙁😕😈 ও তোমাকে ভালোবাসে😇😻 :))"
     }
 			api.sendMessage(msg, threadID, messageID);
 		}
